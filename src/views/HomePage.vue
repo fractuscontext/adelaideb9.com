78e8ad675f76b5a0a0d3df79206a641d627828a3
fractuscontext <106440141+fractuscontext@users.noreply.github.com>
2026-05-16 16:35:25 +0930

    fix(ui): add gap

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -32,7 +32,7 @@ const combinedEvents = computed(() => {
   let now = new Date()
   result = result.filter(i=>now <= new Date(i.date) || now <= new Date(i.end_date));
 
-  upcomingNumber = result.length;
+  upcomingNumber.value = result.length;
 
   // Sort soonest first
   return result.sort((a, b)=>new Date(a.date) - new Date(b.date)).slice(0,3);
@@ -48,44 +48,43 @@ const combinedEvents = computed(() => {
     <div class="space-y-5 mx-10 lg:mx-0 text-lg">
       <h1>We're AdelaideB9 </h1>
       <p class="lg:w-[34rem]">
-        University of Adelaide's competitive cybersecurity team. We win
+        Adelaide University's competitive cybersecurity team. We win
         prizes for pwning servers, reverse-engineering binaries, performing
         forensics on unusually large JPEGs, and much, much more.<br /><br />
 
         If you're a South Australian student who thinks this sounds interesting,
         come join us! We'd love to have you.<br/><br/>
+      </p>
 
-        <span class="m-auto flex flex-row flex-wrap justify-center gap-4 max-w-fit" >
-          <SocialMedia
-            src="/img/social/discord-brands.svg"
-            href="https://discord.gg/M3x2Bkbb3N"
-            :join="true"
-          />
-          <router-link class="no-underline text-s self-center" to="/register">
-            <!-- div class="flex bg-primary border-2 border-current rounded-md px-6 py-2 gap-2">
-              <p class="block font-medium tracking-tighter text-center h-8">
-                Register&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="inline-block stroke-current feather"><line x1="7" y1="17" x2="17" y2="7"/><polyline points="7 7 17 7 17 17"/></svg>
-              </p>
-            </div -->
 
-          <div class="flex backdrop-filter bg-black/10 rounded-md py-3 gap-4 hover:brightness-95">
-            <p class="block font-medium tracking-tighter text-center mx-10 text-nowrap">
-              Register<img src="/img/icons/external-link-dark.svg" class="inline"/>
+      <div class="flex flex-row flex-wrap items-center justify-start gap-4 max-w-fit">
+        <SocialMedia
+          src="/img/social/discord-brands.svg"
+          href="https://discord.gg/M3x2Bkbb3N"
+          :join="true"
+          class="h-[50px] flex items-center" 
+        />
+        
+        <router-link class="no-underline text-s" to="/register">
+          <div class="flex items-center justify-center backdrop-filter bg-black/10 rounded-md h-[50px] px-10 hover:brightness-95 transition-all">
+            <p class="font-medium tracking-tighter text-center m-0 flex items-center gap-2 text-nowrap">
+              Register
+              <img src="/img/icons/external-link-dark.svg" class="w-4 h-4 inline"/>
             </p>
           </div>
-
-          </router-link>
-        </span>
-      </p>
+        </router-link>
+      </div>
     </div>
     <img src="/img/logo.png" class="w-96" />
     <br/>
   </div>
-  <div v-if="combinedEvents.length >= 1 && loaded" class="py-20 -mt-20 flex content-center justify-center items-center gap-12 flex-col-reverse lg:flex-row">
+<div v-if="combinedEvents.length >= 1 && loaded" class="py-20 -mt-20 flex content-center justify-center items-center gap-12 flex-col-reverse lg:flex-row">
     <div class="space-y-5 text-lg">
       <h1 class="text-center" >Upcoming Events</h1>
-      <div v-if="combinedEvents.length >= 1 && loaded" class="flex flex-col gap-4">
-        <div>
+      <div v-if="combinedEvents.length >= 1 && loaded" class="flex flex-col gap-6">
+        
+        <!-- FIX: use 'flex flex-col gap-4' to separate individual cards -->
+        <div class="flex flex-col gap-4">
           <TicketSmall
             v-for="v of combinedEvents"
             :key="v.ID"
